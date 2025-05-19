efca8dd9018812a8d8ad170fab7cc59d9d484411
ritesh <sinharitesh919@gmail.com>
2025-05-19 17:58:41 +0530

    added sentry error boundry

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,9 @@ Sentry.init({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Sentry.ErrorBoundary fallback={<p>Something went wrong</p>}>
+      <App />
+    </Sentry.ErrorBoundary>
   </React.StrictMode>
 );
 
